2dfd50c3ec66daeb470342c7a5f6c563b280dd13
Eduardo Haesbaert <eduardo.haesbaert@gmail.com>
2022-04-21 16:24:55 -0300

    main implementation + avl + node

---
+++ Main.java
@@ -0,0 +1,89 @@
+import tree.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        AVLTree tree = new AVLTree();
+        initProg(tree,true);
+    }
+
+    public static void initProg(AVLTree tree, boolean printH) throws IOException {
+        if (printH) {
+            printHelp();
+            printH = false;
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Insira o comando: ");
+        String input = reader.readLine();
+        String command = input.substring(0,1);
+
+        switch (command) {
+            case "i":
+                tree.root = insertInput(tree, Integer.parseInt(input.substring(2, input.length())));
+                break;
+            case "b":
+                searchInput(tree, Integer.parseInt(input.substring(2, input.length())));
+                break;
+            case "r":
+                tree.root = removeInput(tree, Integer.parseInt(input.substring(2, input.length())));
+                break;
+            case "e":
+                System.exit(0);
+                break;
+            case "h":
+                printH = true;
+                break;
+            case "p":
+                String value = input.substring(2, input.length());
+                if (value.equals("pre")) {
+                    tree.PreOrder(tree.root);
+                } else if (value.equals("post")) {
+                    tree.PostOrder(tree.root);
+                } else if (value.equals("in")) {
+                    tree.InOrder(tree.root);
+                } else {
+                    System.out.println("Comando inválido. Por favor, insira um comando válido, ou pressione h para exibir os comandos.");
+                }
+                break;
+            default:
+                System.out.println("Comando inválido. Por favor, insira um comando válido, ou pressione h para exibir os comandos.");
+        }
+
+        initProg(tree, printH);
+    }
+
+    private static void printHelp() {
+        System.out.println("Para realizar uma operação, digite um dos comandos seguido de um número inteiro, separados por um espaço, e aperte enter:");
+        System.out.println("Inserir(i): i <valor>");
+        System.out.println("Buscar(b): b <valor>");
+        System.out.println("Remover(r): r <valor>");
+        System.out.println("");
+        System.out.println("Print(p):");
+        System.out.println("Pré-ordem: p pre");
+        System.out.println("Pós-ordem: p post");
+        System.out.println("Em ordem: p in");
+        System.out.println("");
+        System.out.println("Para sair, digite e");
+        System.out.println("");
+    }
+
+    private static Node insertInput(AVLTree tree, int value) {
+        return tree.Insert(tree.root, value);
+    }
+
+    private static void searchInput(AVLTree tree, int value) {
+        if (tree.Search(tree.root, value)) {
+            System.out.println("Valor " + value + " encontrado.");
+        } else {
+            System.out.println("Valor " + value + " não encontrado.");
+        }
+    }
+
+    private static Node removeInput(AVLTree tree, int value) {
+        return tree.Delete(tree.root, value);
+    }
+
+}
---
+++ tree/AVLTree.java
@@ -0,0 +1,199 @@
+package tree;
+
+public class AVLTree {
+ 
+    public Node root;
+
+    public Node Insert(Node node, int key) {
+        if (node == null) {
+            System.out.println("Valor " + key + " inserido na arvore.");
+            return (new Node(key));
+        }
+
+        if (key < node.key) {
+            node.left = Insert(node.left, key);
+        } else if (key > node.key) {
+            node.right = Insert(node.right, key);
+        } else {
+            System.out.println("Valor atualmente contido na arvore.");
+            return node;
+        }
+
+        node.height = 1 + maxVal(treeHeight(node.left), treeHeight(node.right));
+
+        int balance = getBalance(node);
+
+        if (balance > 1 && key < node.left.key) {
+            return rotateRight(node);
+        }
+
+        if (balance < -1 && key > node.right.key) {
+            return rotateLeft(node);
+        }
+
+        if (balance > 1 && key > node.left.key) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+
+        if (balance < -1 && key < node.right.key) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+
+        return node;
+    }
+
+    public boolean Search(Node root, int key) {
+        if (searchRec(root, key) == 0){
+            return false;
+        }
+        return true;
+    }
+
+    private int searchRec(Node node, int key) {
+        if (node == null)
+            return 0;
+
+        System.out.println("Nodo consultado: " + node.key);
+        if (key < node.key) {
+            return searchRec(node.left, key);
+        } else if (key > node.key) {
+            return searchRec(node.right, key);
+        } else {
+            return node.key;
+        }
+    }
+
+    public Node Delete(Node root, int key) {
+        if (root == null) {
+            System.out.println("Arvore vazia.");
+            return root;
+        }
+
+        if (key < root.key) {
+            root.left = Delete(root.left, key);
+        } else if (key > root.key) {
+            root.right = Delete(root.right, key);
+        } else {
+            if ((root.left == null) || (root.right == null)) {
+                Node nodeTmp = null;
+
+                if (nodeTmp == root.left) {
+                    nodeTmp = root.right;
+                }else {
+                    nodeTmp = root.left;
+                }
+
+                if (nodeTmp == null) {
+                    nodeTmp = root;
+                    root = null;
+                } else {
+                    root = nodeTmp;
+                }
+            } else {
+                Node nodeTmp = minValueNode(root.right);
+                root.key = nodeTmp.key;
+                root.right = Delete(root.right, nodeTmp.key);
+            }
+        }
+
+        if (root == null) {
+            return root;
+        }
+
+        root.height = maxVal(treeHeight(root.left), treeHeight(root.right)) + 1;
+
+        int balance = getBalance(root);
+
+        if (balance > 1 && getBalance(root.left) >= 0) {
+            return rotateRight(root);
+        }
+
+        if (balance > 1 && getBalance(root.left) < 0) {
+            root.left = rotateLeft(root.left);
+            return rotateRight(root);
+        }
+
+        if (balance < -1 && getBalance(root.right) <= 0) {
+            return rotateLeft(root);
+        }
+
+        if (balance < -1 && getBalance(root.right) > 0) {
+            root.right = rotateRight(root.right);
+            return rotateLeft(root);
+        }
+
+        return root;
+    }
+
+    public void PreOrder(Node node) {
+
+    }
+
+    public void PostOrder(Node node) {
+
+    }
+
+    public void InOrder(Node node) {
+
+    }
+
+    int treeHeight(Node node) {
+        if (node == null) {
+            return 0;
+        }
+
+        return node.height;
+    }
+ 
+    int maxVal(int a, int b) {
+        return (a > b) ? a : b;
+    }
+ 
+    Node rotateRight(Node nodeA) {
+        Node nodeB = nodeA.left;
+        Node nodeTmp = nodeB.right;
+
+        nodeB.right = nodeA;
+        nodeA.left = nodeTmp;
+
+        nodeA.height = maxVal(treeHeight(nodeA.left), treeHeight(nodeA.right)) + 1;
+        nodeB.height = maxVal(treeHeight(nodeB.left), treeHeight(nodeB.right)) + 1;
+ 
+        return nodeB;
+    }
+ 
+    Node rotateLeft(Node nodeA) {
+        Node nodeB = nodeA.right;
+        Node nodeTmp = nodeB.left;
+
+        nodeB.left = nodeA;
+        nodeA.right = nodeTmp;
+
+        nodeA.height = maxVal(treeHeight(nodeA.left), treeHeight(nodeA.right)) + 1;
+        nodeB.height = maxVal(treeHeight(nodeB.left), treeHeight(nodeB.right)) + 1;
+
+        return nodeB;
+    }
+ 
+    int getBalance(Node node) {
+        if (node == null) {
+            return 0;
+        }
+
+        return (treeHeight(node.left) - treeHeight(node.right));
+    }
+
+
+
+    Node minValueNode(Node node) {
+        Node current = node;
+
+        while (current.left != null) {
+            current = current.left;
+        }
+
+        return current;
+    }
+}
