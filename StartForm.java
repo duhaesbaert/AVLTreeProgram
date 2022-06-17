b9cd188e5d22ee93f04563ca1bd6136ba72d0904
Eduardo Haesbaert <eduardo.haesbaert@gmail.com>
2022-06-16 23:51:40 -0300

    start on ui

---
+++ StartForm.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class StartForm {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextArea sejaBemVindoATextArea;
+    private JButton button1;
+    private JTextField textField1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
